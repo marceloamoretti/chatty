0d9544423ee4e7683c6622b8ffa9466402b3de0e
Marcelo <marceloamoretti@gmail.com>
2023-06-24 14:12:41 -0400

    feat: setup theme

---
+++ src/components/ui/theme.ts
@@ -0,0 +1,23 @@
+import { Theme } from './types';
+
+export const theme: Theme = {
+  colors: {
+    primary: '#10A37F',
+    secondary: '#FFFFFF',
+    bgPrimary: '#343541',
+    bgSecondary: '#000000',
+  },
+  fontFamily: {
+    semibold: 'BarlowSemiCondensed-SemiBold',
+    medium: 'BarlowSemiCondensed-Medium',
+    regular: 'BarlowSemiCondensed-Regular',
+  },
+  fontSizes: {
+    large: 32,
+    regular: 16,
+  },
+  borderRadius: {
+    radiusSquare: 0,
+    radiusRounded: 8,
+  },
+};
